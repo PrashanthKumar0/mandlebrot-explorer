d91c840a6f615816a38ec390e131ce2b20c1e7eb
prashanthKumar0 <31966594+PrashanthKumar0@users.noreply.github.com>
2024-03-21 00:34:36 +0530

    Added glad

--- src/Window.cpp
+++ src/Window.cpp
@@ -1,3 +1,4 @@
+#include <glad/glad.h>
 #include <GLFW/glfw3.h>
 #include <string>
 #include <stdexcept>
@@ -6,13 +7,15 @@
 
 mbe::Window::Window(int width, int height, const std::string &title)
 {
-    if (!glfwInit())
+    if (glfwInit() != GLFW_TRUE)
     {
         throw std::runtime_error("Failed Initializing glfw");
     }
+
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
 
     mGLFWwindow = glfwCreateWindow(width, height, title.c_str(), nullptr, nullptr);
 
@@ -22,6 +25,13 @@ mbe::Window::Window(int width, int height, const std::string &title)
     }
 
     glfwMakeContextCurrent(mGLFWwindow);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        throw std::runtime_error("Glad failed finding opengl instance (opengl32.dll)");
+    }
+
+    glViewport(0, 0, width, height);
 }
 
 bool mbe::Window::isOpen()
--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,10 @@ int main()
     constexpr int window_height{640};
     mbe::Window window{window_width, window_height, "Mandlebrot Explorer."};
 
+    glClearColor(0.1f, 0.2f, 0.2f, 1.0f);
     while (window.isOpen())
     {
-        // ok
+        glClear(GL_COLOR_BUFFER_BIT);
         window.sync();
     }
 }
